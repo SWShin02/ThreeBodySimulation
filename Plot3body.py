451c72a3488f42bf5f5b6b845aa2b05fd9b17a66
SWShin02 <69069253+SWShin02@users.noreply.github.com>
2025-02-19 18:07:49 +0900

    Add files via upload

---
+++ Plot3body.py
@@ -0,0 +1,26 @@
+import matplotlib.pyplot as plt
+from modules.CoordinateTransformation import load_data_3body_rot as load_data
+
+# Load data
+filename = 'threebodyL4'
+data = load_data(filename, update=True)
+# data = data.loc[:2000]
+
+
+# Layout
+fig = plt.figure(figsize=(10, 10))
+ax = fig.add_subplot(111)
+
+# Plot
+ax.scatter(data['x1'], data['y1'], label='Sun', c='r')
+ax.scatter(data['x2'], data['y2'], label='Jupiter', c='b')
+ax.plot(data['x3'], data['y3'], label='particle', c='g')
+
+# Details
+ax.axis('equal')
+ax.set_xlabel('x')
+ax.set_ylabel('y')
+ax.legend()
+
+# Save
+fig.savefig(f'{filename}.png')
---
+++ threebodyL4.py
@@ -0,0 +1,55 @@
+import numpy as np
+import pandas as pd
+import scipy.constants as const
+from modules.CoordinateTransformation import rotate, CM
+from modules.NbodySimulation import RK4
+
+# Constants
+M_sun = 1.989e30 # [kg]
+G = (const.G * const.day**2 / const.au**3) * M_sun # [AU^3 day^-2 M_sun^-1]
+
+# M_earth = 5.972e24/M_sun    # [M_sun]
+M_Jupiter = 1.898e27/M_sun  # [M_sun]
+
+r_Jupiter = 5.2 # [AU]
+
+# Initial conditions
+m = np.array([1e-2, 1e-2, 1e-3])
+
+r_vec = np.array([[0, 0], [r_Jupiter, 0], [0, 0]])
+r_vec[2] = rotate(np.pi/3, r_vec[1])
+r_vec = r_vec - CM(m, r_vec)
+
+a = r_Jupiter
+w = np.sqrt(G * m.sum() / a**3)
+
+v_vec = np.zeros((3, 2))
+angular_peturbation = 0
+for i in range(3):
+    v_vec[i] = rotate(np.pi/2, r_vec[i]) * w
+    v_vec[i] = rotate(angular_peturbation, v_vec[i])
+v_vec[2] = v_vec[2]*1.02
+
+# Simulation parameters
+dt = 2 # [day]
+iterations = 100000
+
+# Dataframe
+data = pd.DataFrame({
+    'x1': np.zeros(iterations),
+    'y1': np.zeros(iterations),
+    'x2': np.zeros(iterations),
+    'y2': np.zeros(iterations),
+    'x3': np.zeros(iterations),
+    'y3': np.zeros(iterations),
+    'theta': np.zeros(iterations)
+})
+
+# Simulation
+theta = 0
+for i in range(iterations):
+    r_vec, v_vec = RK4(m, r_vec, v_vec, dt, Gravity_constant=G)
+    theta = (theta + w*dt) % (2*np.pi)
+    data.loc[i] = np.array([r_vec[0, 0], r_vec[0, 1], r_vec[1, 0], r_vec[1, 1], r_vec[2, 0], r_vec[2, 1], theta])
+
+data.to_csv('./data/threebodyL4.csv', index=False)
